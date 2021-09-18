567cb2ff7d3a2251a850de19082ecd31bc9a18dc
Vahe Sevachyan <vscode13@gmail.com>
2021-09-17 23:40:05 -0700

    updated algorithm and added some temporary styling

--- regex.js
+++ regex.js
@@ -6,3 +6,76 @@ function hasWhiteSpace(value) {
 function hasWhiteSpace(value) {
   return /^\s*$/.test(value);
 }
+
+//checks if its a valid url
+function validURL(url) {
+  let pattern = new RegExp(
+    "^(https?:\\/\\/)?" + // protocol
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+      "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+      "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+      "(\\#[-a-z\\d_]*)?$",
+    "i"
+  ); // fragment locator
+  return !!pattern.test(url);
+}
+
+const tearApartUrl = (url) => {
+  if (url.includes(indexOf(".")) && url.includes(lastIndexOf("."))) {
+    let match = url
+      .substring(0, url.lastIndexOf("."))
+      .substring(url.indexOf(".") + 1);
+    return match;
+  }
+  if (url.includes(indexOf(".") + 1)) {
+    let match = url.substring(url.indexOf(".") + 1);
+    return match;
+  }
+  if (url.includes(lastIndexOf("."))) {
+    let match = url.substring(0, url.lastIndexOf("."));
+    return match;
+  }
+};
+
+function wHostName(value) {
+  return /[\w+]+\:\/\/)?([\w\d-]+\.)*[\w-]+[\.\:]\w+([\/\?\=\&\#\.]?[\w-]+)*\/?/gm.test(
+    value
+  );
+}
+
+let regex =
+  /([\w+]+\:\/\/)?([\w\d-]+\.)*[\w-]+[\.\:]\w+([\/\?\=\&\#\.]?[\w-]+)*\/?/gm;
+
+const stripUrl = (url) => {
+  let match = url.replace(
+    /^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/gim,
+    ""
+  );
+  return match;
+};
+
+function getHostName(url) {
+  // let match = url.match(
+  //   /[\w+]+\:\/\/)?([\w\d-]+\.)*[\w-]+[\.\:]\w+([\/\?\=\&\#\.]?[\w-]+)*\/?/gm
+  // );
+  let match = url.match(/:\/\/(www[0-9]?\.)?(.[^/:]+)/i);
+
+  if (
+    match != null &&
+    match.length > 2 &&
+    typeof match[2] === "string" &&
+    match[2].length > 0
+  ) {
+    let hostname = match[2].split(".");
+    return hostname[0];
+  }
+}
+////strips any URL BOMBAYYY
+////strips any URL BOMBAYYY
+const stripUrl = (url) => {
+  let match = url.replace(/.+\/\/|www.|\..+/g, "");
+  return match;
+};
+////strips any URL BOMBAYYY
+////strips any URL BOMBAYYY
--- script.js
+++ script.js
@@ -3,22 +3,18 @@ const domainInput = document.querySelector("#domainName");
 const generateBtn = document.querySelector(".generate");
 const resetBtn = document.querySelector(".reset");
 
-// empty string or string with white space
-// function hasWhiteSpace(value) {
-//   return /^$|\s+/.test(value);
-// }
-
 //regex for empty string or just white space
 function hasWhiteSpace(value) {
   return /^\s*$/.test(value);
 }
-
 generateBtn.addEventListener("click", (e) => {
   e.preventDefault();
   validate(phraseInput, domainInput);
 });
 //validates if empty string or white spaces only
+
 const validate = (phrase, url) => {
+  // checkLength(phrase);
   if (
     hasWhiteSpace(phrase.value) === true ||
     hasWhiteSpace(url.value) === true
@@ -38,31 +34,111 @@ const trim = (phrase, url) => {
   const urlValue = url.value.replace(/ /g, "");
   lowerCase(secretPhrase, urlValue);
 };
-
 //converts all to lower case
 const lowerCase = (phrase, url) => {
   const secretPhrase = phrase.toLowerCase();
-  const urlValue = url.toLowerCase();
-  lengthFunction(secretPhrase, urlValue);
-  console.log(secretPhrase, urlValue);
+  let urlValue = url.toLowerCase();
+  scramble(secretPhrase, stripUrl(urlValue));
+  // domainCheck(secretPhrase, urlValue);
+};
+//add a display message informing the user which domain was provided but which part of was used and for future reference they can only use strip down version.
+
+//strips down url
+const stripUrl = (url) => {
+  let match = url.replace(/.+\/\/|www.|\..+/g, "");
+  return match;
 };
 
 //adds numbers to begging and end of string
-const lengthFunction = (phrase, url) => {
-  let phraseLength = phrase.length;
-  const urlLength = url.length;
+const scramble = (phrase, url) => {
+  let passPhrase = phrase.split("").reverse();
+  let URL = url.split("").reverse();
+  const phraseLength = phrase.length * 7;
+  const urlLength = url.length * 11;
   const combinedLength = urlLength * phraseLength;
-  let secretPhrase = combinedLength + phrase.concat(phraseLength, urlLength);
-  // secretPhrase = combinedLength + secretPhrase;
-  // secretPhrase = combinedLength + phrase.concat(phraseLength, urlLength);
-  //  secretPhrase =
-
-  // secretPhrase = secretPhrase.split("").reverse();
-  console.log(secretPhrase);
-  //   secretPhrase = phraseLength + secretPhrase;
-  // indexFunction(secretPhrase, url);
-  //   console.log(secretPhrase);
+  let phraseIndex0 = passPhrase[0];
+  // URL[0] = phraseIndex0.toUpperCase();
+  URL[1] = phraseIndex0.toUpperCase();
+  let urlLastIndex = URL[URL.length - 1];
+  // let urlSecLastIndex = URL[URL.length - 2];
+  passPhrase[0] = urlLastIndex.toUpperCase();
+  // passPhrase[1] = urlSecLastIndex.toLowerCase();
+  let obliterated = [
+    phraseLength,
+    "$",
+    ...passPhrase,
+    "@",
+    combinedLength,
+    "#",
+    ...URL,
+    "!",
+    urlLength,
+    "%",
+  ].join("");
+  // upperCase(obliterated);
+  console.log(obliterated);
 };
+
+//refactor bottom two reverse functions into one forEach
+// const reverseValues = (url, phrase) => {
+//   const URL = url.split("").reverse().join("");
+//   const passPhrase = phrase.split("").reverse().join("");
+// };
+// const reverseURL = (url) => {
+//   const URL = url.split("").reverse().join("");
+//   if (URL.length <= 2) {
+//     const newURL = URL.slice(1) + url.slice(0, 1);
+//     console.log(newURL);
+//     // let finishedURL = newURL.split("").join("");
+//     return newURL;
+//   }
+//   if (URL.length >= 3) {
+//     let newURL = URL.slice(3) + URL.slice(0, 3);
+//     // let finishedURL = newURL.split("").join("");
+//     console.log(newURL);
+//     // console.log(newURL);
+//     return newURL;
+//   }
+// };
+// const reversePhrase = (phrase) => {
+//   const passPhrase = phrase.split("").reverse();
+//   if (passPhrase.length <= 2) {
+//     const newPhrase = passPhrase.slice(1) + passPhrase.slice(0, 1);
+//     // let finishedPhrase = newPhrase.split("").join("");
+//     // console.log(newPhrase);
+//     return newPhrase;
+//   }
+//   if (passPhrase.length >= 3) {
+//     const newPhrase = passPhrase.slice(3) + passPhrase.slice(0, 3);
+//     console.log(newPhrase);
+//     // let finishedPhrase = newPhrase.split("").join("");
+//     return newPhrase;
+//   }
+// };
+// let newPhrase = "!" + passPhrase.slice(0, 3) + "$" + passPhrase.slice(3);
+// newPhrase = newPhrase.split("");
+// const toUpperCase = (url) => {
+//   let URL = url;
+//   for (let i = 0; i < URL.length; i += 2) {
+//     let UcaseUrl = URL[i].toUpperCase;
+//     return UcaseUrl;
+//   }
+// };
+
+// const upperCase = (phrase) => {
+//   let passPhrase = phrase;
+//   for (let i = 0; i < passPhrase.length; i += 2) {
+//     if (passPhrase[i].typeof === "string") {
+//       return passPhrase[i].toUpperCase;
+//     }
+//     console.log(passPhrase);
+//   }
+// };
+
+// const injectingValues = (phrase) => {
+//   let passPhrase = phrase;
+//   passPhrase = passPhrase[2].toUpperCase();
+// };
 
 // const indexFunction = (phrase, url) => {
 //   let index0ofDomain = url.charAt(0).toLowerCase();
@@ -78,6 +154,22 @@ const lengthFunction = (phrase, url) => {
 //   reverse(secretPhrase);
 // };
 
+// let secretPhrase = combinedLength + phrase.concat(phraseLength, urlLength);
+// secretPhrase = combinedLength + phrase.concat(phraseLength, urlLength);
+//  secretPhrase =
+// secretPhrase = secretPhrase.split("").reverse();
+// console.log(secretPhrase);
+//   secretPhrase = phraseLength + secretPhrase;
+// indexFunction(secretPhrase, url);
+//   console.log(secretPhrase);
+// empty string or string with white space
+// function hasWhiteSpace(value) {
+//   return /^$|\s+/.test(value);
+// }
+
+// function consoleLog(value) {
+//   console.log(value);
+// }
 // const reverse = (phrase) => {
 //   let revPhrase = phrase.reverse().join("");
 //   revPhrase[0] + `#${phrase.length * 2}`;
